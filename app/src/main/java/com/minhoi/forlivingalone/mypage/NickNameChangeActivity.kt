3587a5825b615ae4a204865d89103bc9f6168cd0
MinhoiKoo <minlove2013@gmail.com>
2023-05-09 23:52:36 +0900

    [fix] 사용자 닉네임 변경

--- app/src/main/java/com/minhoi/forlivingalone/mypage/NickNameChangeActivity.kt
+++ app/src/main/java/com/minhoi/forlivingalone/mypage/NickNameChangeActivity.kt
@@ -2,16 +2,31 @@ package com.minhoi.forlivingalone.mypage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.minhoi.forlivingalone.R
 import com.minhoi.forlivingalone.databinding.ActivityNickNameChangeBinding
 
 class NickNameChangeActivity : AppCompatActivity() {
     private lateinit var binding : ActivityNickNameChangeBinding
+    private lateinit var viewModel: NickNameChangeViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_nick_name_change)
+        viewModel = ViewModelProvider(this).get(NickNameChangeViewModel::class.java)
+        binding.changeNickVM = viewModel
 
+        binding.btnChangeNickName.setOnClickListener {
+            viewModel.changeBtnClicked()
+        }
+
+        viewModel.isValidNickName.observe(this) {
+            if(it) {
+                finish()
+                Toast.makeText(this, "닉네임 변경 완료", Toast.LENGTH_LONG).show()
+            }
+        }
 
 
 
--- app/src/main/java/com/minhoi/forlivingalone/mypage/NickNameChangeViewModel.kt
+++ app/src/main/java/com/minhoi/forlivingalone/mypage/NickNameChangeViewModel.kt
@@ -3,23 +3,29 @@ package com.minhoi.forlivingalone.mypage
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.minhoi.forlivingalone.utils.Ref
 
 class NickNameChangeViewModel : ViewModel() {
 
-    private val _nickName = MutableLiveData<String>()
-    val nickName : LiveData<String>
-        get() = _nickName
-
-    private val currentUserUid = Ref.auth.currentUser?.uid
-    private val currentNickName = currentUserUid.get
+    private val currentUserRef = Ref.userRef.child(Ref.auth.currentUser?.uid.toString())
+    val nickName = MutableLiveData<String>()
 
 
-    fun clickChangeNickBtn() : Int {
-        if(currentNickName.equals(_nickName)) {
-            return 2
-        }
-        return 1
+    var isValidNickName  = MutableLiveData(false)
+
+    fun changeBtnClicked() {
+        val map = hashMapOf<String,Any>()
+        map.put("nickname", nickName.value.toString())
+        currentUserRef.updateChildren(map)
+        isValidNickName.value = true
     }
+
+
+
+
 
 }
